bddd89cb90f84453ea6db3505743560e9a8edd24
itc-n23026 <n23026@std.it-college.ac.jp>
2023-12-20 05:51:21 +0000

    Chapter4.5
    Containerコンポーネントで横幅を整える

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Container from 'components/container'
 import Hero from 'components/Hero'
 
 const Home = () => {
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+  return (
+    <Container>
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    </Container>
+  )
 }
 
 export default Home
